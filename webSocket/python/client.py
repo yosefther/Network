6f18dea321b50c102ef1e5fe3624fa33463f3da1
yosefther <yousefthair9@gmail.com>
2026-04-07 20:25:59 +0300

    fix : added the missing code

--- webSocket/python/client.py
+++ webSocket/python/client.py
@@ -2,7 +2,7 @@ import socket
 
 IP =  '127.0.0.1'.encode("utf-8")
 PORT = 9090
-Date= "Hello from UDP".encode("utf-8")
+Data= "Hello from UDP".encode("utf-8")
 
-sock.sendto(Date, (IP , PORT))
-
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.sendto(Data, (IP , PORT) )
